3d121eafa8a64dc4a2eabbca0093f0c83a095046
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-11-18 15:04:32 -0600

    navbar links and routes

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -5,12 +5,15 @@ import LocationCard from './location/LocationCard';
 import EmployeeCard from './employee/EmployeeCard';
 import OwnerCard from './owner/OwnerCard';
 import NavBar from './nav/NavBar';
+import ApplicationViews from './ApplicationViews';
 
 class Kennel extends Component {
     render() {
         return (
-            
-            <NavBar />
+            <React.Fragment>
+                <NavBar />
+                <ApplicationViews />
+            </React.Fragment>
         );
     }
 }
