c555a796cc742f6bce162ed0ca5a130c6f520585
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-11-18 14:19:49 -0600

    second commit

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react'
 import './Kennel.css'
 import AnimalCard from './animal/AnimalCard'
+import LocationCard from './location/LocationCard';
+import EmployeeCard from './employee/EmployeeCard';
+import OwnerCard from './owner/OwnerCard';
+import NavBar from './nav/NavBar';
 
 class Kennel extends Component {
     render() {
         return (
-            <div>
-                <div>
-                <h2>Student Kennels<br />
-                    <small>Loving care when you're not there.</small>
-                </h2>
-                <address>
-                    Visit Us at the Nashville North Location
-                    <br />500 Puppy Way
-                </address>
-            </div>
-            <div>
-                <AnimalCard />
-                <AnimalCard />
-                <AnimalCard />
-            </div>
-            </div>
+            
+            <NavBar />
         );
     }
 }
