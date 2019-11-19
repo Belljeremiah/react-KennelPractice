5f91eb4ba0a90beea2ab09adc46448b4fedd4957
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-11-19 09:39:44 -0600

    working in class on practice api

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,11 +1,11 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from 'react'
 import Home from './home/Home'
-import AnimalCard from './animal/AnimalCard'
 //only include these once they are built - previous practice exercise
 import LocationCard from './location/LocationCard'
 import EmployeeCard from './employee/EmployeeCard'
 import OwnerCard from './owner/OwnerCard'
+import AnimalList from './animal/AnimalList'
 
 
 class ApplicationViews extends Component {
@@ -17,7 +17,7 @@ class ApplicationViews extends Component {
           return <Home />
         }} />
         <Route path="/animals" render={(props) => {
-          return <AnimalCard />
+          return <AnimalList />
         }} />
         <Route path="/locations" render={(props) => {
             return <LocationCard />
